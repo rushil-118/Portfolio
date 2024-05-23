04aeced8fa8bd283b795139af2d552d69d6dad2d
Rushil Choudhary <choudharyrushil118@gmail.com>
2024-05-23 22:26:41 +0530

    initial commit

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import './About.css';
+function About(){
+    return(
+        <div className="about">
+            <div>
+                <h2>Who I am</h2>
+                <p>My name's Rushil. I'm a Student at Scaler School of Technology.
+                    <br>
+                    </br>
+
+                </p>
+            </div>
+        </div>
+    )
+}
+export default About;
---
+++ src/Education.js
@@ -0,0 +1,27 @@
+import './Education.css';
+
+function Education(){
+    return(
+        <div className="education">
+            <h1>Education</h1>
+            <div className='education-details'>
+                <div>
+                    <div>
+                        <h6>2016-2023</h6>
+                        <h4>Higher Secondary Education</h4>
+                        <h5>Narayana E-Techno School</h5>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h6>2023-Present</h6>
+                        <h4>Bachelor of Science</h4>
+                        <h5>Scaler School of Technology</h5>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+export default Education;
